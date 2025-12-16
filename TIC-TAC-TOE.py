a2fdae8ed737850256cb2a25c6ee56df0704c559
hm24csb1a20 <hm24csb1a20@student.nitw.ac.in>
2025-12-16 19:01:56 +0530

    made the version much faster with numpy and integer allocations

--- TIC-TAC-TOE.py
+++ TIC-TAC-TOE.py
@@ -1,48 +1,45 @@
+import numpy as np
+board = np.zeros((3,3),dtype=int)
 
-print("hello world")
-
-
-board = [["E","E","E"],["E","E","E"],["E","E","E"]]
-winning = [
-    [0,1,2],[3,4,5],[6,7,8],  # rows
-    [0,3,6],[1,4,7],[2,5,8],  # cols
-    [0,4,8],[2,4,6]           # diagonals
-]
+# changing the manual looping to just cheking the sum
+def winning(b):
+    if np.any(np.sum(b,axis=0)==3) or np.any(np.sum(b,axis=0) == -3):
+        return True
+    elif np.any(np.sum(b,axis=1)==3) or np.any(np.sum(b,axis=1) == -3):
+        return True
+    
+    # diagonals 
+    if abs(np.trace(b))==3 or abs(np.trace(b.T))==3:
+        return True
+    
+    return False
 
 print("""
 0 1 2 
 3 4 5
 6 7 8
 """)
-marker = "X"
+marker = 1
+symbols = {
+    1: "X",
+    -1: "O",
+    0: "E"
+}
 while True:
-    print(f"as the {marker} what do u want to play:")
+    print(f"as the {symbols[marker]} what do u want to play:")
     c = int(input("enter the index no of what u want to select: "))
     
     row_in = c // 3
     col_in = c % 3
-    
+    if(board[row_in][col_in]!= 0):
+        print("cell used already")
+        continue
     board[row_in][col_in]=marker
-    wins= True
-    # now to check if tying or winning 
-    for i in winning:
-        wins= True
-        for j in i :
-            r_in  = int(j//3)
-            c_in = int(j%3)
-            if board[r_in][c_in]==marker:
-                continue
-            else:
-                wins = False
-        if(wins):
-            print("U win yay")
-            break
-    if(wins):
+
+    if(winning(board)):
+        print(f"player {symbols[marker]} has won")
         break
     for i in board:
         print(i)
         print("",end="")
-    if marker =="X":
-        marker = "O"
-    else:
-        marker = "X"
+    marker *=-1
